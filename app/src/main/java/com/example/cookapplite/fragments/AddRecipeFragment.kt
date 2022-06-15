bd75c22340285af8d0ceff3aeed9b87d310999d3
nicobortoni <nicobortoni@frba.utn.edu.ar>
2022-06-15 16:40:49 -0300

    Add user modificado - firebase storage incluido

--- app/src/main/java/com/example/cookapplite/fragments/AddRecipeFragment.kt
+++ app/src/main/java/com/example/cookapplite/fragments/AddRecipeFragment.kt
@@ -3,6 +3,7 @@ package com.example.cookapplite.fragments
 import android.app.Activity
 import android.app.Activity.RESULT_OK
 import android.content.Intent
+import android.net.Uri
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import android.provider.MediaStore
@@ -10,11 +11,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import com.example.cookapplite.R
 import com.example.cookapplite.databinding.AddRecipeFragmentBinding
 import com.example.cookapplite.databinding.AddUserFragmentBinding
 import com.example.cookapplite.viewmodels.AddRecipeViewModel
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 
 class AddRecipeFragment : Fragment() {
 
@@ -22,6 +26,8 @@ class AddRecipeFragment : Fragment() {
 
     private lateinit var _binding : AddRecipeFragmentBinding
     private val binding get() = _binding!!
+
+    private var recipeImage :Uri? = null
 
     companion object {
         fun newInstance() = AddRecipeFragment()
@@ -43,22 +49,32 @@ class AddRecipeFragment : Fragment() {
 
     override fun onStart() {
         super.onStart()
+        val storage = Firebase.storage
+        val storageReference = storage.reference
 
         binding.pickImageBtn.setOnClickListener {
             //Gallery intent
-            val gallery = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+            val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
             resultLauncher.launch(gallery)
         }
 
-        binding.saveInStorageBtn.setOnClickListener{
+        binding.saveInStorageBtn.setOnClickListener {
             //Storage image in database
+            val recipesImagesRef = storageReference.child("recipesImages/image1")
+            val uploadTask = recipesImagesRef.putFile(recipeImage!!)
+
+            uploadTask.addOnFailureListener {
+                Toast.makeText(requireContext(),"error",Toast.LENGTH_SHORT).show()
+            }.addOnSuccessListener { taskSnapshot ->
+                Toast.makeText(requireContext(),"uploaded",Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
     private var resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
         if (result.resultCode == Activity.RESULT_OK) {
-            val data : Intent? = result.data
-            binding.imageView.setImageURI(data?.data)
+            recipeImage = result.data?.data
+            binding.imageView.setImageURI(recipeImage)
         }
     }
 }
